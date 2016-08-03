0e34bc28a95d3a509ae35fa4e0d51df24eb13ade
dianafmendez <diana.mendez@students.dbcr.org>
2016-08-03 15:54:21 -0400

    Add files via upload

---
+++ idkwhatimdoing.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function whatisgoingon () {
+var dict = []; // create an empty array
+dict.push({
+    key:   "username",
+    value : "password"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var dict = []; // create an empty array
+
+
+
+//var username = key
+//var passwrd = value
+
+//dict.push({
+  //  key:   "username",
+   // value : "password"
+// });
+
+
+
+//var friends = TAFFY([
+//	{"id":1,"gender":"M","first":"John","last":"Smith","city":"Seattle, WA","status":"Active"},
+//	{"id":2,"gender":"F","first":"Kelly","last":"Ruth","city":"Dallas, TX","status":"Active"},
+//	{"id":3,"gender":"M","first":"Jeff","last":"Stevenson","city":"Washington, D.C.","status":"Active"},
+//	{"id":4,"gender":"F","first":"Jennifer","last":"Gill","city":"Seattle, WA","status":"Active"}	
+//]);
